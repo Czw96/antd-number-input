0e3c8a62be0a5eea14d3c5758259ba104e4bf135
Czw <36317241+Czw96@users.noreply.github.com>
2026-06-01 21:36:54 +0800

    2026-06-01

--- eslint.config.js
+++ eslint.config.js
@@ -3,20 +3,20 @@ import eslintConfigPrettier from "eslint-config-prettier";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import { defineConfig, globalIgnores } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  { ignores: ["dist", "node_modules"] },
+export default defineConfig([
+  globalIgnores(["dist", "node_modules"]),
   {
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       ...tseslint.configs.recommended,
       react.configs.flat.recommended,
       react.configs.flat["jsx-runtime"],
-      eslintConfigPrettier,
     ],
-    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -42,5 +42,6 @@ export default tseslint.config(
     rules: {
       "react-refresh/only-export-components": "off",
     },
-  }
-);
+  },
+  eslintConfigPrettier,
+]);
